10a6ca0ae5b234d4550d6c85bbab2792fa70aae6
ShaunZap <sjbzappy@gmail.com>
2025-03-19 12:27:18 +0530

    test2

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 const users = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page");
+});
 
 app.get("/api/v1/users", (req, res) => {
   res
     .status(200)
     .json({ status: "success", results: users.length, data: { users } });
 });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
